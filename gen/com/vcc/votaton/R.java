eb247020f59c57815a3baa54e23482cfca162c45
jdllamas <jotallamas@gmail.com>
2014-05-03 11:40:01 -0500

    Commit Boton
    Se prueba el boton para funcionamiento.

--- gen/com/vcc/votaton/R.java
+++ gen/com/vcc/votaton/R.java
@@ -1000,6 +1000,7 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1016,7 +1017,6 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageButton1=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1148,6 +1148,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int button_partido=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
     }
     public static final class style {
--- src/com/vcc/votaton/MainActivity.java
+++ src/com/vcc/votaton/MainActivity.java
@@ -9,19 +9,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
 	}
 
 	@Override
@@ -59,6 +62,10 @@ public class MainActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+	
+	public void showPartidos(View view) {
+		Toast.makeText(this.getApplicationContext(), "Entro al envento de Partidos", Toast.LENGTH_SHORT).show();
 	}
 
 }
