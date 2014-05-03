f484eeeb923346337561b3a39383f13eb9aad95a
jdllamas <jotallamas@gmail.com>
2014-05-03 11:47:48 -0500

    Prueba con AppCompat (Solo)
    Prueba con AppCompat (Solo)

--- src/com/vcc/votaton/MainActivity.java
+++ src/com/vcc/votaton/MainActivity.java
@@ -1,7 +1,6 @@
 package com.vcc.votaton;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,9 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
-import android.os.Build;
+
 
 public class MainActivity extends ActionBarActivity {
 	
