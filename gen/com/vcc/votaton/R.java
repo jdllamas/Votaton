405e142caca746ee8da6304c89ea4f5824069f15
jdllamas <jotallamas@gmail.com>
2014-05-03 15:52:44 -0500

    Descripcion
    Descripcion

--- gen/com/vcc/votaton/R.java
+++ gen/com/vcc/votaton/R.java
@@ -983,8 +983,10 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int button_estadisticas_fondo=0x7f020057;
         public static final int button_partidos_fondo=0x7f020058;
-        public static final int ic_launcher=0x7f020059;
-        public static final int partidos_politicos=0x7f02005a;
+        public static final int button_votos_fondo=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
+        public static final int partidos_politicos=0x7f02005b;
+        public static final int vote=0x7f02005c;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
